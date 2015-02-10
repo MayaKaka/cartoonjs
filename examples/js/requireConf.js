d22769f2fdf3da79cfb674785a370f8cd83a5597
chenrui09 <chenrui09@baidu.com>
2015-02-10 17:05:11 +0800

    del examples
    del examples

--- examples/js/requireConf.js
+++
@@ -1,9 +0,0 @@
-require.config({ 
-	baseUrl: '/src', 
-	paths: {
-		'cartoon': 'cartoon',
-		'cartoon.filters': 'cartoon.filters',
-		'cartoon.partilces': 'cartoon.partilces',
-		'cartoon.three': 'cartoon.three'
-	}
-});
